39047aebc203f244eece05ac66b473cbad6673b2
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-06-30 20:08:08 +0200

    add api call

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,26 @@
-<h1>flight tracker test</h1>
-
 <script>
-import { onMount } from "svelte";
-import { apiData, flights } from './store.js';
+	import { onMount } from 'svelte';
+	import { apiData, flights } from './store.js';
 
-onMount (async () => {
-    console.log('calling flight data API')
-    const result = fetch('http://localhost:8000/flights')
-    .then(
-        response => response.json()
-        .then(
-            jsonResponse => {
-                console.log(jsonResponse)
-                apiData.set(jsonResponse)
-            }
-        )
-    )
-    return result
-})
+	onMount(async () => {
+		console.log('calling flight data API');
+		const result = fetch('http://localhost:8000/flights').then((response) =>
+			response.json().then((jsonResponse) => {
+				console.log(jsonResponse);
+				apiData.set(jsonResponse);
+			})
+		);
+		return result;
+	});
 </script>
 
+<h1>flight tracker test</h1>
+
 <main>
-	<h1>Flights</h1>
+	<h2>Flights</h2>
 	<ul>
-	{#each $flights as flight}
-		<li>{flight}</li>
-	{/each}
+		{#each $flights as flight}
+			<li>{flight}</li>
+		{/each}
 	</ul>
 </main>
-
-
