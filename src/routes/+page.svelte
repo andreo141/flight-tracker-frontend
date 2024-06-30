81e4d2a19991a2bd0a228c76e9636f3a7c41f079
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-06-30 16:46:38 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<h1>flight tracker test</h1>
+
+<script>
+import { onMount } from "svelte";
+import { apiData, flights } from './store.js';
+
+onMount (async () => {
+    console.log('calling flight data API')
+    const result = fetch('http://localhost:8000/flights')
+    .then(
+        response => response.json()
+        .then(
+            jsonResponse => {
+                console.log(jsonResponse)
+                apiData.set(jsonResponse)
+            }
+        )
+    )
+    return result
+})
+</script>
+
+<main>
+	<h1>Flights</h1>
+	<ul>
+	{#each $flights as flight}
+		<li>{flight}</li>
+	{/each}
+	</ul>
+</main>
+
+
